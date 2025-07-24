31785fa29227ba3c6766b5cb5959de0dce1bec89
Ayush Soni <soni.ayush.2212@gmail.com>
2025-07-25 00:06:22 +0530

    Add: /auth/google/callback

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import "dotenv/config"
+import "dotenv/config";
 
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 
@@ -9,8 +9,8 @@ import { authRouter } from "./routes/auth.routes";
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use("/auth", authRouter)
+app.use(cors());
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,11 +3,18 @@ import { randomBytes } from "crypto";
 
 const authRouter = Router();
 
+const stateStore = new Map();
+
 authRouter.get("/google", (req, res) => {
   // Create query params for the request and redirect
 
   // State to secure CSRF attacks
   const state = randomBytes(32).toString("hex");
+
+  stateStore.set(state, {
+    timestamp: Date.now(),
+    userId: req.query.userId || null,
+  });
 
   const authURL = new URL(process.env.GOOGLE_AUTH_URL!);
 
@@ -20,6 +27,58 @@ authRouter.get("/google", (req, res) => {
   authURL.searchParams.append("prompt", "consent");
 
   res.redirect(authURL.toString());
+});
+
+authRouter.get("/google/callback", async (req, res) => {
+  try {
+    const { code, state, error } = req.query;
+
+    console.log("Code: ", code);
+    console.log("State: ", state);
+    console.log("Error: ", error);
+
+    if (error) throw new Error("Oauth error");
+    if (!code || !state) throw new Error("Missing required paramaters");
+
+    const store = stateStore.get(state);
+    if (!store) throw new Error("Invalid or expired state parameter");
+
+    if (Date.now() - store.timestamp > 5 * 60 * 1000) {
+      stateStore.delete(state);
+      res
+        .json(400)
+        .json({ success: false, message: "State parameter expired" });
+    }
+
+    stateStore.delete(state);
+
+    // We exchange the authorization code for access token
+
+    const params = new URLSearchParams();
+    params.append("client_id", process.env.GOOGLE_CLIENT_ID!);
+    params.append("client_secret", process.env.GOOGLE_CLIENT_SECRET!);
+    params.append("code", code as string);
+    params.append("grant_type", "authorization_code");
+    params.append("redirect_uri", process.env.REDIRECT_URI!);
+
+    const response = await fetch(process.env.GOOGLE_TOKEN_URL!, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params.toString(),
+    });
+
+    if (!response.ok) throw new Error("Error during token exchange");
+
+    const result = await response.json();
+    const { access_token, refresh_token, expires_in, token_type } = result;
+
+    // Push the refresh to DB and use access to log in
+
+  } catch (error: any) {
+    res.status(500).json({ success: false, message: error });
+  }
 });
 
 export { authRouter };
